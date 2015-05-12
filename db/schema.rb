be0be7b5796bb127d5a44692bb5c2d44f7f8a03c
Helio Oliveira <hheelliioo@live.com>
2015-05-11 21:34:02 -0300

    Change db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150429230702) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dojos", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
@@ -26,7 +29,7 @@ ActiveRecord::Schema.define(version: 20150429230702) do
     t.datetime "updated_at",     null: false
   end
 
-  add_index "dojos", ["user_id"], name: "index_dojos_on_user_id"
+  add_index "dojos", ["user_id"], name: "index_dojos_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -43,7 +46,8 @@ ActiveRecord::Schema.define(version: 20150429230702) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "dojos", "users"
 end
